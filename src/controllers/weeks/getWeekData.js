eced15f49267f19983683c6c34dc84668379e5a6
oleksii ponomarenko <alexponomarenko996@gmail.com>
2026-04-25 08:12:06 +0200

    update code

---
+++ src/controllers/weeks/getWeekData.js
@@ -0,0 +1,21 @@
+export const getWeekData = (req, res) => {
+  const { weekNumber } = req.params;
+
+  const weekNumberNum = Number(weekNumber);
+
+  if (isNaN(weekNumberNum) || weekNumberNum < 1 || weekNumberNum > 40) {
+    return res.status(400).json({ message: "Invalid week number" });
+  }
+
+  const data = {
+    week: weekNumberNum,
+    daysToBirth: 165,
+    baby: {
+      size: "як авокадо",
+      description: "Ваш малюк активно розвивається",
+    },
+    momTip: "Пийте більше води та відпочивайте",
+  };
+
+  res.json(data);
+};
